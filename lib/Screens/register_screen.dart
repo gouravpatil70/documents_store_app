c6179df3821f0f88a64824f1cf9c3de8657beedd
gourav <gouravpatil075@gmail.com>
2024-06-19 09:27:19 +0530

    Worked on UI design of the register screen

--- lib/Screens/register_screen.dart
+++ lib/Screens/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:documents_store_app/Widgets/custom_app_bar.dart';
+import 'package:documents_store_app/Widgets/custom_button.dart';
+import 'package:documents_store_app/Widgets/custom_input_box.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget{
@@ -14,6 +16,73 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       appBar: CustomAppBar.customAppBar(context: context, appTitle: 'Register User'),
       
+      body: Center(
+        child: Card(
+          margin: const EdgeInsets.all(10.0),
+          elevation: 3.0,
+          shadowColor: Colors.black,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+
+                // Register UI Icon
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                  child: CircleAvatar(
+                    radius: 45.0,
+                    child: Icon(
+                      Icons.person_add_alt,
+                      color: Colors.grey[700],
+                      size: 40.0,
+                    ),
+                  ),
+
+                ),
+
+                // Name Field
+                const CustomTextFormField(validator: 'Please provide name', labelText: 'Name', icon: Icons.person_2_outlined, isPasswordField: false),
+
+                // Email Field
+                const CustomTextFormField(validator: 'Please provide Email Id', labelText: 'Email Id', icon: Icons.mail_outline, isPasswordField: false),
+
+                // Password Field
+                const CustomTextFormField(validator: 'Please enter Password', labelText: 'Password', icon: Icons.password_outlined, isPasswordField: true),
+
+                // Register Button
+                CustomButton.customOutlinedButton(context: context, buttonText: 'Register Me'),
+
+                // If Account Exist text
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.all(10.0),
+                  child: GestureDetector(
+                    child: const Text(
+                      'Already have an Account ?',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontStyle: FontStyle.italic,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                    
+                    // OnClick event
+                    onTap: ()=> _navigateToLoginScreen(),
+                  ),
+                ),
+
+
+                // You can add other widgets from here ...
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+
+  // Navigate to Login Screen
+  _navigateToLoginScreen(){
+    Navigator.pushReplacementNamed(context, '/loginScreen');
   }
 }
