bbe1a519637c6387e92f2cfde2f3b8d8a03b7f28
gourav <gouravpatil075@gmail.com>
2024-06-23 17:08:39 +0530

    Fixing the space bug in upload_documents_scree.dart and did some changes

--- lib/Screens/upload_documents_Screen.dart
+++ lib/Screens/upload_documents_Screen.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:documents_store_app/Model/DocumentMode.dart';
 import 'package:documents_store_app/Screens/home_screen.dart';
 import 'package:documents_store_app/Widgets/custom_alert_dialog.dart';
 import 'package:documents_store_app/Widgets/custom_app_bar.dart';
@@ -8,8 +10,15 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 
+// Firebase packages
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class UploadDocuments extends StatefulWidget {
-  const UploadDocuments({super.key});
+  final User user;
+  const UploadDocuments({super.key, required this.user});
+
+
 
   @override
   State<UploadDocuments> createState() => _UploadDocumentsState();
@@ -20,9 +29,19 @@ class _UploadDocumentsState extends State<UploadDocuments> {
   // Requried instances
   final TextEditingController _editingDocumentNameController = TextEditingController();
 
+
   // Requried Variables
   File? _document;
   String? _documentName;
+
+  // Firebase Storage reference
+  final FirebaseFirestore _cloudStorage = FirebaseFirestore.instance;
+
+  // Instance of the Document model
+  final DocumentStoreModel _documentStoreModel = DocumentStoreModel.initializeWithEmptyValues();
+
+  // Form Key
+  final GlobalKey<FormState> _formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -32,44 +51,46 @@ class _UploadDocumentsState extends State<UploadDocuments> {
         child: Card(
           elevation: 3.0,
           shadowColor: Colors.black,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-            
-                // For Document Name
-                CustomTextFormField(validator: 'Please provide document Name', labelText: 'Document Name', icon: Icons.edit_document, isPasswordField: false,editingController: _editingDocumentNameController, callBack: _onChangedDocumentNameValue),
-            
-                // Selecting document from the gallery
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: GestureDetector(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 80, 164, 232),
-                        borderRadius: BorderRadius.circular(10.0)
-                      ),
-                      width: MediaQuery.of(context).size.width,
-                      padding: const EdgeInsets.all(15.0),
-                      margin: const EdgeInsets.all(10.0),
-                      child: Center(
-                        child: Text(
-                          _documentName != null ? _documentName! : 'Select Document',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 17.0
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+              
+                  // For Document Name
+                  CustomTextFormField(validator: 'Please provide document Name', labelText: 'Document Name', icon: Icons.edit_document, isPasswordField: false,editingController: _editingDocumentNameController, callBack: _onChangedDocumentNameValue),
+              
+                  // Selecting document from the gallery
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: GestureDetector(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 80, 164, 232),
+                          borderRadius: BorderRadius.circular(10.0)
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(15.0),
+                        margin: const EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Text(
+                            _documentName != null ? _documentName! : 'Select Document',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 17.0
+                            ),
+                            overflow: TextOverflow.ellipsis,
                           ),
-                          overflow: TextOverflow.ellipsis,
                         ),
                       ),
+                      onTap: ()=>_pickFileFromStorage(),
                     ),
-                    onTap: ()=>_pickFileFromStorage(),
                   ),
-                ),
-
-
-                CustomButton.customElevatedButton(context: context, buttonText: 'Upload',callBack: _uploadDocumentToDatabase),
-              ],
+            
+                  CustomButton.customElevatedButton(context: context, buttonText: 'Upload',callBack: _uploadDocumentToDatabase),
+                ],
+              ),
             ),
           ),
         ),
@@ -81,6 +102,10 @@ class _UploadDocumentsState extends State<UploadDocuments> {
   _onChangedDocumentNameValue(String input){
     setState(() {
       _editingDocumentNameController.text = input;
+
+      // Stroing documentName into the Document Model variable
+      _documentStoreModel.documentName = input.trim();
+
     });
   }
 
@@ -97,14 +122,34 @@ class _UploadDocumentsState extends State<UploadDocuments> {
     }
   }
 
-  // upload document to cloud storage
+  // Upload document to cloud storage
   _uploadDocumentToDatabase()async{
-    bool resultOfAlertDialog = await CustomAlertDialog.customAelrtDialogBox(context: context, message: 'Document Successfully Uploaded');
+    if(_formKey.currentState!.validate()){
+      _formKey.currentState!.save();
+      
+      // Uploading file to storage
+      try{
+        Reference storageReference = FirebaseStorage.instance.ref().child(_documentName!);
+        await storageReference.putFile(_document!).then((firebaseFile)async{
+          var downloadUrl = await firebaseFile.ref.getDownloadURL();
+          setState(() {
+            _documentStoreModel.documentUrl = downloadUrl;
+          });
+        });
 
-    if(resultOfAlertDialog){
-      Timer(const Duration(milliseconds: 500),(){
-        _navigateToHomeScreen();
-      });
+        // Now Upload data into the firestore database. Specifically to users Account
+        await _cloudStorage.collection('userDetails').doc(widget.user.email).collection('documents').doc(_documentName).set(_documentStoreModel.toJson(), SetOptions(merge: true));
+
+        bool resultOfAlertDialog = await _showErrorMessage('File uploaded successfully');
+
+        if(resultOfAlertDialog){
+          Timer(const Duration(milliseconds: 500),(){
+            _navigateToHomeScreen();
+          });
+        }
+      }catch(e){
+        _showErrorMessage(e.toString());
+      }
     }
   }
 
@@ -116,5 +161,9 @@ class _UploadDocumentsState extends State<UploadDocuments> {
     }), (route) => false);
   }
   
+  Future<bool> _showErrorMessage(String message)async{
+    bool result = await CustomAlertDialog.customAelrtDialogBox(context: context, message: message);
+    return result;
+  }
 }
 
