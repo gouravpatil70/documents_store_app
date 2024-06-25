b2cdc2aaf6d85741ed70314f29d68fef7c773827
gourav <gouravpatil075@gmail.com>
2024-06-25 10:20:44 +0530

    Removed the download functionality from the documents_list_screen

--- lib/Screens/documents_list_screen.dart
+++ lib/Screens/documents_list_screen.dart
@@ -32,8 +32,6 @@ class _ViewDocumentState extends State<ViewDocument> {
   Map<String,dynamic>? _mapObject;
   bool? isLoading;
   bool isDownloading = false;
-  double downloadProgress = 0.0;
-  int? _bottomIndexForProgressBar;
 
   // Dio instance
   final Dio dio = Dio();
@@ -43,10 +41,10 @@ class _ViewDocumentState extends State<ViewDocument> {
     super.initState();
     isLoading = true;
     _mapObject = {};
-    _bottomIndexForProgressBar = 0;
     getDocumentsOfTheUser();
   }
   
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,8 +68,8 @@ class _ViewDocumentState extends State<ViewDocument> {
                     ),
                   )
                 )
-                :
-                Padding(
+                
+                : Padding(
                   padding: const EdgeInsets.only(left: 10.0, right: 10.0,bottom: 7.0),
                   child: Card(
                     elevation: 2.5,
@@ -79,7 +77,6 @@ class _ViewDocumentState extends State<ViewDocument> {
                     child: Column(
                       children: [
                         ListTile(
-                          // tileColor: const Color.fromARGB(255, 230, 234, 237),
                           tileColor: Colors.white,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10.0),
@@ -87,7 +84,6 @@ class _ViewDocumentState extends State<ViewDocument> {
                           title: Text(
                             _mapObject![(_mapObject!.keys).toList().asMap()[index]]['documentName'],
                             style: const TextStyle(
-                              // color: Colors.white,
                               fontWeight: FontWeight.bold
                             ),
                             overflow: TextOverflow.ellipsis,
@@ -114,16 +110,6 @@ class _ViewDocumentState extends State<ViewDocument> {
                         
                           onTap: ()=>_navigateToDocumentFile(mapObjectKey: (((_mapObject!.keys).toList())[index]), mapObjectValue: _mapObject![(((_mapObject!.keys).toList())[index])]),
                         ),
-                        _bottomIndexForProgressBar != index+1
-                        ? const SizedBox()
-                        : Padding(
-                            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                            child: LinearProgressIndicator(
-                              backgroundColor: const Color.fromARGB(255, 230, 234, 237),
-                              value: downloadProgress == 1.0 ? 0.0 : downloadProgress,
-                              valueColor: const AlwaysStoppedAnimation(Colors.red),
-                            ),
-                          )                        
                       ],
                     ),
                   ),
@@ -133,6 +119,8 @@ class _ViewDocumentState extends State<ViewDocument> {
         ),
     );
   }
+
+
 
   // Getting the data from the firestore.
   getDocumentsOfTheUser()async{
@@ -144,10 +132,11 @@ class _ViewDocumentState extends State<ViewDocument> {
       }
       setState(() {
         isLoading = false;
-        print(_mapObject);
+        // print(_mapObject);
       });
     });
   }
+
 
   // Navigate to Document File
   _navigateToDocumentFile({required String mapObjectKey ,required Map<String,dynamic> mapObjectValue})async{
@@ -160,102 +149,10 @@ class _ViewDocumentState extends State<ViewDocument> {
     );
   }
 
-  // Downloading the file from the storage
-  downloadFile({required String url, required String fileName})async{
-    setState((){
-      isDownloading = true;
-    });
-
-    String result = await saveFile(fileUrl: url,fileName: fileName);
-    
-    _showDialogBox(result);
-
-    setState((){
-      isDownloading = false;
-    });
-
+  _deleteFileFromCloudStorage(){
+    _showDialogBox('File Deleted from Cloud Storage');
   }
 
-  // Saving file into directory
-  Future<String> saveFile({required String fileUrl, required String fileName})async{
-    Directory? directory;
-    try{
-      // Platform method came from dart:io.
-      if(Platform.isAndroid){   
-        // For Android
-        if(await _requestpermission(Permission.manageExternalStorage)){
-          directory = await getExternalStorageDirectory();
-
-          // Creating the another folder inside the Android folder          
-          directory = getDirectoryPath(directory);
-          print(directory!.path);
-
-        }else{
-          return "Permission Not Granted";
-        }
-
-      }else{
-        // For IoS
-      }
-
-      // Check whether the directory is created or not 
-      if(!await directory!.exists()){
-        await directory.create(recursive: true);
-      }
-
-      if(await directory.exists()){
-        File saveFile = File('${directory.path}/$fileName');
-        await dio.download(fileUrl, saveFile.path, onReceiveProgress: (downloaded, totalSize){
-          setState(() {
-            downloadProgress = downloaded/totalSize;
-            if(downloadProgress == 1.0){
-              _bottomIndexForProgressBar = 0;
-            }
-          });
-        });
-
-        return "File Downloaded Successfully";
-      }
-    }catch(e){
-      return e.toString();
-    }
-    return "File Not Downloaded";
-  }
-
-  // Getting the directory path where we want to store the file
-  Directory? getDirectoryPath(Directory? directory){
-
-    String newPath = '';
-    List<String> folders = directory!.path.split('/');
-    for(int i=0;i<folders.length;i++){
-      String folder = folders[i];
-      if(folder != 'Android'){
-        newPath += '$folder/';
-      }else{
-        break;
-      }
-    }
-
-    // Now create a another directory or folder inside the Android
-    newPath = '${newPath}Android/DocumentStoreApp';
-    directory = Directory(newPath);
-    // print(directory.path);
-    return directory;
-  }
-
-  // Checking whether the permission is given by the user or not
-  Future<bool> _requestpermission(Permission permission)async{
-    if(await permission.isGranted){
-      return true;
-    }else{
-      PermissionStatus result = await permission.request();   // permission.request() return the permission status
-      if(result.isGranted){
-        return true;
-      }else{
-        return false;
-      }
-    }
-  }
 
   // Showing an dialogBox
   _showDialogBox(String message){
