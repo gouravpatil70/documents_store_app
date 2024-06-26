0152bd487860d74de2d5caa42ce7fd7c5e2a0e36
gourav <gouravpatil075@gmail.com>
2024-06-26 11:43:44 +0530

    disabled the email input field for not changing the email Id and did some other changes

--- lib/Screens/modify_user_details_screen.dart
+++ lib/Screens/modify_user_details_screen.dart
@@ -1,13 +1,19 @@
 import 'dart:async';
-
+import 'package:documents_store_app/Model/UserDetailsModel.dart';
 import 'package:documents_store_app/Screens/home_screen.dart';
 import 'package:documents_store_app/Widgets/custom_alert_dialog.dart';
 import 'package:documents_store_app/Widgets/custom_app_bar.dart';
 import 'package:documents_store_app/Widgets/custom_button.dart';
 import 'package:documents_store_app/Widgets/custom_input_box.dart';
 import 'package:flutter/material.dart';
+
+// Firebase Packages
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ModifyUserDetails extends StatefulWidget {
-  const ModifyUserDetails({super.key});
+  final User user;
+  const ModifyUserDetails({super.key, required this.user});
 
   @override
   State<ModifyUserDetails> createState() => _ModifyUserDetailsState();
@@ -20,6 +26,30 @@ class _ModifyUserDetailsState extends State<ModifyUserDetails> {
   final TextEditingController _editingNameController = TextEditingController();
   final TextEditingController _editingEmailIdController = TextEditingController();
   final TextEditingController _editingPasswordController = TextEditingController();
+
+  // Required fields or attributes
+  bool? _isLoading;
+  bool? _isDataModified;
+
+
+  // Updating the _auth display Name
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  
+  // Creating instance of the firestore database
+  final FirebaseFirestore _cloudFirestore = FirebaseFirestore.instance;
+  String? _userEmailIdFromAuth;
+
+  // Create an instance of the UserDetailsModel 
+  final UserDetailsModel _userDetailsModel = UserDetailsModel.initializeWithEmptyValues();
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    _isDataModified = false;
+    _userEmailIdFromAuth = widget.user.email;
+    _getUserDetailsForModification();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,13 +66,13 @@ class _ModifyUserDetailsState extends State<ModifyUserDetails> {
                 children: [
 
                   // Name Field
-                  CustomTextFormField(validator: 'Please provide Name', labelText: 'Name', icon: Icons.person, isPasswordField: false, editingController: _editingNameController,callBack: _onChangedNameValue),
+                  CustomTextFormField(validator: 'Please provide Name', isInputBoxEnabled: true, labelText: 'Name', icon: Icons.person, isPasswordField: false, editingController: _editingNameController,callBack: _onChangedNameValue),
                   
                   // Email Id Field
-                  CustomTextFormField(validator: 'Please provide Email Id', labelText: 'Email Id', icon: Icons.email_outlined, isPasswordField: false, editingController: _editingEmailIdController,callBack: _onChangedEmailValue),
+                  CustomTextFormField(validator: 'Please provide Email Id', isInputBoxEnabled: false, labelText: 'Email Id', icon: Icons.email_outlined, isPasswordField: false, editingController: _editingEmailIdController,callBack: _onChangedEmailValue),
 
                   // Password Field
-                  CustomTextFormField(validator: 'Please enter Passwrod', labelText: 'Password', icon: Icons.password_outlined, isPasswordField: true, editingController: _editingPasswordController,callBack: _onChangedPasswordValue),
+                  CustomTextFormField(validator: 'Please enter Passwrod', isInputBoxEnabled: true, labelText: 'Password', icon: Icons.password_outlined, isPasswordField: true, editingController: _editingPasswordController,callBack: _onChangedPasswordValue),
 
                   // Modify Button 
                   CustomButton.customElevatedButton(context: context, buttonText: 'Modify',callBack: _modifyUserDetailsCallBack),
@@ -55,34 +85,101 @@ class _ModifyUserDetailsState extends State<ModifyUserDetails> {
     );
   }
 
+
+  // Getting the User Details
+  _getUserDetailsForModification()async{
+
+    // getting the User Details
+    DocumentSnapshot<Map<String,dynamic>> userDetailsSnapshot = await _cloudFirestore.collection('userDetails').doc(_userEmailIdFromAuth).get();
+
+    Map<String, dynamic>? userDetailsData = userDetailsSnapshot.data();
+
+    if(userDetailsData != null){
+      // Stroing values to the User Model attributes
+      _userDetailsModel.userName = userDetailsData['userName'];
+      _userDetailsModel.userEmailId = userDetailsData['userEmailId'];
+      _userDetailsModel.userPassword = userDetailsData['userPassword'];
+
+      // Adding values to the textEditingController
+      setState(() {
+        _editingNameController.text = userDetailsData['userName'];
+        _editingEmailIdController.text = userDetailsData['userEmailId'];
+        _editingPasswordController.text = userDetailsData['userPassword'];
+
+        // Turning off the isLoading
+        _isLoading = false;
+      });
+    }
+  }
+
   // For Name
   _onChangedNameValue(String input){
     setState(() {
       _editingNameController.text = input;
+
+      // Updating the User Model name attribute
+      _userDetailsModel.userName = input.trim();
+
+      // Check Condition if data modified
+      _isDataModified = true;
     });
   }
 
   // For Email
   _onChangedEmailValue(String input){
-    setState(() {
-      _editingEmailIdController.text = input;
-    });
+    // setState(() {
+    //   _editingEmailIdController.text = input;
+
+    //   // Updating the User Model email attribute
+    //   _userDetailsModel.userEmailId = input.trim();
+
+    //   // Check Condition if data modified
+    //   _isDataModified = true;
+    // });
   }
 
   // For Password
   _onChangedPasswordValue(String input){
     setState(() {
       _editingPasswordController.text = input;
+
+      // Updating the User Model password attribute
+      _userDetailsModel.userPassword = input.trim();
+
+      // Check Condition if data modified
+      _isDataModified = true;
     });
   }
 
   // Modify Details Button
   _modifyUserDetailsCallBack()async{
-    bool resultOfAlertDialog = await CustomAlertDialog.customAelrtDialogBox(context: context, message: 'Details Modified');
-    if(resultOfAlertDialog){
-      Timer(const Duration(milliseconds: 500), (){ 
-        _navigateToHomeScreen();
-      });
+
+    // Work Pending Upladingj the details
+    if(_isDataModified!){
+      try{
+
+        // Updating the database
+        await _cloudFirestore.collection('userDetails').doc(_userEmailIdFromAuth).update(_userDetailsModel.toJson());
+        
+        // Updating the User Display Name Inside the Authentication 
+        _auth.authStateChanges().listen((event) {
+          event!.updateProfile(
+            displayName: _userDetailsModel.userName
+          );
+        });
+
+        bool resultOfAlertDialog = await _showErrorDialogBox('Details Modified Successfully');
+        if(resultOfAlertDialog){
+          Timer(const Duration(milliseconds: 500), (){ 
+            _navigateToHomeScreen();
+          });
+        }
+
+      }catch(e){
+        _showErrorDialogBox(e.toString());
+      }
+    }else{
+      _showErrorDialogBox('Data Not Modified');
     }
   }
 
@@ -90,8 +187,14 @@ class _ModifyUserDetailsState extends State<ModifyUserDetails> {
   _navigateToHomeScreen(){
     Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
       builder: (BuildContext context){
-        return HomeScreen();
+        return const HomeScreen();
       }
     ), (route) => false);
+  }
+
+  Future<bool> _showErrorDialogBox(String message)async{
+      bool resultOfAlertDialog = await CustomAlertDialog.customAelrtDialogBox(context: context, message: message);
+      return resultOfAlertDialog;
+
   }
 }
