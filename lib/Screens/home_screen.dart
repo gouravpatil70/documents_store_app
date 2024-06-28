c7c66a61416135c62c198706a517aeb6b238f486
gourav <gouravpatil075@gmail.com>
2024-06-28 12:13:26 +0530

    Worked on displaying the recent documents upened on UI

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,14 +1,26 @@
-import 'dart:async';
 
+// Screens Package
 import 'package:documents_store_app/Screens/modify_user_details_screen.dart';
+import 'package:documents_store_app/Screens/show_document.dart';
 import 'package:documents_store_app/Screens/upload_documents_screen.dart';
 import 'package:documents_store_app/Screens/documents_list_screen.dart';
+
+// Widget Packages
 import 'package:documents_store_app/Widgets/custom_alert_dialog.dart';
 import 'package:documents_store_app/Widgets/custom_app_bar.dart';
 import 'package:documents_store_app/Widgets/drawer_card_widget.dart';
+
+// Flutter default packages
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+// Permission Handler
 import 'package:permission_handler/permission_handler.dart';
+
+// Firebase Packages
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,9 +35,16 @@ class _HomeScreenState extends State<HomeScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   User? _user;
 
+  // Required Variables
+  Map<int,dynamic>? _recentDocuments;
+
+  // Firestore Package
+  final FirebaseFirestore _cloudFirestore = FirebaseFirestore.instance;
+
   @override
-  void initState() {
+  void initState(){
     super.initState();
+    _recentDocuments = {};
     checkAuthentication();
   }
   
@@ -125,9 +144,10 @@ class _HomeScreenState extends State<HomeScreen> {
             // Recently Opened Applications
             Expanded(
               child: ListView.builder(
-                itemCount: 1,
+                itemCount: _recentDocuments!.isNotEmpty ? _recentDocuments!.length : 1,
                 itemBuilder: (BuildContext context, int index){
-                  return Card(
+                  return _recentDocuments!.isEmpty
+                  ? Card(
                     elevation: 3.0,
                     child: Container(
                       padding: const EdgeInsets.all(25.0),
@@ -144,7 +164,45 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                     ),
-                  );
+                  )
+                  : GestureDetector(
+                      child: Card(
+                        elevation: 3.0,
+                        child: Container(
+                          padding: const EdgeInsets.all(20.0),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 92, 175, 242),
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  width: 220.0,
+                                  child: Text(
+                                    _recentDocuments![index]['docId'],
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18.0
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  
+                                ),
+                                const Icon(
+                                  Icons.file_copy_sharp,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      onTap: (){
+                        _getDocumentDetails(id: _recentDocuments![index]['docId']);
+                      },
+                    );
                 }
               )
             )
@@ -156,7 +214,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   // Checking Authtication 
   checkAuthentication(){
-    _auth.authStateChanges().listen((user) { 
+    _auth.authStateChanges().listen((user) async { 
       
       if(user == null){
         if(mounted){
@@ -164,31 +222,33 @@ class _HomeScreenState extends State<HomeScreen> {
         }
       }else{
         if(mounted){
-          setState(() {
+          setState((){
             _user = user;
             print(_user!.displayName);
             print(_user);
           });
-          Timer(const Duration(seconds: 2),(() {
-            checkAndAskStoragePermission();
+          Timer(const Duration(seconds: 2),(()async{
+            await checkAndAskStoragePermission();
           }));
+          _checkRecentlyOpenedDocuments();
         }
       }
     });
   }
 
-
   checkAndAskStoragePermission()async{
-    if(await _showDialogBoxForPermission('This app needs the storage permission')){
-      Permission permission = Permission.manageExternalStorage;
-      if(await permission.isGranted){
-        return true;
-      }else{
-        PermissionStatus permissionAccessResult = await permission.request();
-        if(permissionAccessResult.isGranted){
+    Permission permission = Permission.manageExternalStorage;
+    if(await permission.isDenied){
+      if(await _showDialogBoxForPermission('This app needs the storage permission')){
+        if(await permission.isGranted){
           return true;
-        }else if(permissionAccessResult.isDenied){
-          checkAndAskStoragePermission();
+        }else{
+          PermissionStatus permissionAccessResult = await permission.request();
+          if(permissionAccessResult.isGranted){
+            return true;
+          }else if(permissionAccessResult.isDenied){
+            checkAndAskStoragePermission();
+          }
         }
       }
     }
@@ -197,5 +257,34 @@ class _HomeScreenState extends State<HomeScreen> {
   _showDialogBoxForPermission(String message){
     Future<dynamic> result = CustomAlertDialog.customAelrtDialogBox(context: context, message: message);
     return result;
+  }
+
+
+  _checkRecentlyOpenedDocuments()async{
+    var result = _cloudFirestore.collection('userDetails').doc(_user!.email);
+    int index=0;
+    Map<int,dynamic> mapObject = {};
+    result.collection('recentOpenedDocuments').get().then((value){
+      for(var item in value.docs){
+        mapObject[index] = item.data();
+        index++;
+      }
+      setState(() {
+        _recentDocuments = mapObject;
+      });
+    });
+  }
+
+  _getDocumentDetails({required String id})async{
+    var result = await _cloudFirestore.collection('userDetails').doc(_user!.email).collection('documents').doc(id).get();
+    _navigateToTheDocumentScreen(id: result.id, data: result.data());
+  }
+
+  _navigateToTheDocumentScreen({required String id,required Map<String,dynamic>? data}){
+    Navigator.push(context, MaterialPageRoute(
+      builder: (BuildContext context){
+        return ShowDocument(user: _user!, documentDetails: {id:data});
+      }
+    ));
   }
 }
